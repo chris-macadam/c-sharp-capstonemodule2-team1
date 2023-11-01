1ecfd77433ec1d1114a1403a220fb3bceeefcab3
Mac Reske <richard.m.reske@Gmail.com>
2023-11-01 12:50:56 -0400

    Transaction Model Created

---
+++ TenmoServer/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace TenmoServer.Models
+{
+    public class Transaction
+    {
+        public int TransferType { get; set; }
+        public int TransferStatus { get; set; }
+        public int AccountFromId { get; set; }
+        public string? AccountFromName { get; set; }
+        public int AccountToId { get; set; }
+        public string? AccountToName { get; set; }
+        public decimal TransactionAmount { get; set; }
+    }
+}
