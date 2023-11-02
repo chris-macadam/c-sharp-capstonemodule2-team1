b83f6de450149aa3ba79d39c58953177fa64dd51
Chris <macadamchris@gmail.com>
2023-11-02 09:26:47 -0400

    syncing

---
+++ TenmoServer/Controllers/TransferController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [Route("transfer")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private ITransferDao transferDao;
+
+        public TransferController (ITransferDao transferDao)
+        {
+            this.transferDao = transferDao;
+        }
+    }
+}
